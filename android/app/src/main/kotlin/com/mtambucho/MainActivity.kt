421b7d06b06fbca69d92c8ea27ad6caefc8b328b
Martin Tambucho <82840488+martintambucho@users.noreply.github.com>
2023-09-20 09:29:14 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/mtambucho/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mtambucho.challenge_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:challenge_bloc/app/app.dart';
+import 'package:challenge_bloc/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
